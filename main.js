a04839220065e8eecc6398dac6999d6feb70226c
Shpak Markiian bc2025-2 <shpakmark@gmail.com>
2025-09-12 20:46:59 +0300

    a program for constructing URL was written

---
+++ main.js
@@ -0,0 +1,10 @@
+const currency = 'UAH';
+const exact_date = '10.09.2024';
+
+let nbuURL = new URL('https://bank.gov.ua/NBU_ovdp');
+
+nbuURL.searchParams.append('json', '');
+nbuURL.searchParams.append('val_code', currency);
+nbuURL.searchParams.append('date', exact_date);
+
+console.log(nbuURL.href);
